88facb051cd6d19c0f5280d9d5f29dd4308f00d6
AYUSH <128342781+syntaxXayush@users.noreply.github.com>
2025-09-19 23:09:43 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -19,14 +19,14 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
      cors: {
-    origin: "http://localhost:5173", // production में specific domain डालना
+    origin: "https://zingo.onrender.com", 
     methods: ["GET", "POST"],
     credentials: true  
   }
 })
 app.set("io", io);
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://zingo.onrender.com",
     credentials:true
 }))
 app.use(express.json())
